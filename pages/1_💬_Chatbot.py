ce87a019f4c6b76824cd33c03668d3b250065acb
Gesmast <sheikhshameer99@gmail.com>
2026-02-17 15:22:04 +0500

    Update 1_💬_Chatbot.py

--- pages/1_💬_Chatbot.py
+++ pages/1_💬_Chatbot.py
@@ -142,11 +142,11 @@ if prompt := st.chat_input("How can I help you today?"):
                     t_name = tool_call["name"]
                     t_args = tool_call["args"]
                     
-                    if t_name == "scrape_dataquartz":
+                    if t_name == "get_company_info":
                         observation = scrape_dataquartz(**t_args)
                     else:
                         selected_tool = next(t for t in st.session_state.tools if t.name == t_name)
-                        if t_name == "create_cal_booking":
+                        if t_name == "create_booking":
                             t_args["session_id"] = st.session_state.session_id
                         # Executing async MCP tools via sync bridge
                         observation = asyncio.run(selected_tool.ainvoke(t_args))
